e4184ac5c266c3cf205dc3b9c1e26229c107a085
Nguyen Long <nguyenlong232253@gmail.com>
2022-06-05 16:56:50 +0700

    done show list

---
+++ src/main/java/com/codegym/service/Impl/ProvinceService.java
@@ -0,0 +1,36 @@
+package com.codegym.service.Impl;
+
+import com.codegym.model.Province;
+import com.codegym.repository.ProvinceRepository;
+import com.codegym.service.IProvinceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ProvinceService implements IProvinceService {
+    @Autowired
+    ProvinceRepository provinceRepository;
+
+
+    @Override
+    public Iterable<Province> findAll() {
+        return provinceRepository.findAll();
+    }
+
+    @Override
+    public Optional<Province> findById(Long id) {
+        return provinceRepository.findById(id);
+    }
+
+    @Override
+    public void save(Province province) {
+        provinceRepository.save(province);
+    }
+
+    @Override
+    public void remove(Long id) {
+        provinceRepository.deleteById(id);
+    }
+}
